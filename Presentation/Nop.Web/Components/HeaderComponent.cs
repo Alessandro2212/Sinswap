1016e536faaf247e77fd9968a7e1e30cd4ade295
Alessandro Zelli <alessandro.zelli@jumbo.com>
2022-09-15 22:07:05 +0200

    header component

---
+++ Presentation/Nop.Web/Components/HeaderComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Web.Components
+{
+    public class HeaderViewComponent : NopViewComponent
+    {
+        public HeaderViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
